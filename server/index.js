758da4e0990b2ed8386113751ab3fb89a4af05e5
AmaraMtshali <2662283@students.wits.ac.za>
2025-04-24 23:57:56 +0200

    fix: missing modules

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
-require('dotenv').config({path:'./config.env'});
+require('dotenv').config({path:'.env'});
 const UserModel = require('./models/sportsbooking')
 
 const app = express()
